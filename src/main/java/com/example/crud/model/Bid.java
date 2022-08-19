d8ec1dabdecd0643cb6b1ac99a1d4f59ff0a6d58
o0alvin0o <o0cesc0o@gmail.com>
2022-08-19 15:55:58 +0700

    Fix mapping entities model

--- src/main/java/com/example/crud/model/Bid.java
+++ src/main/java/com/example/crud/model/Bid.java
@@ -7,18 +7,20 @@ import lombok.Setter;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.OneToOne;
+import java.io.Serializable;
 import java.time.LocalDateTime;
 
 @Getter @Setter
 @Entity @NoArgsConstructor
-public class Bid {
+public class Bid implements Serializable {
 
+    @OneToOne @Id
+    private User user;
     @OneToOne
     private Product product;
     private LocalDateTime actionTime;
     private double bidPrice;
-    @OneToOne @Id
-    private User user;
+
 
 
 }
